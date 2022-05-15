c1ff55919047613e3c9350fdff8258498349751e
natnael-OQP <makiya112545@gmail.com>
2022-05-15 21:10:02 +0300

    compelete this component

--- components/PinComponent.tsx
+++ components/PinComponent.tsx
@@ -1,16 +1,40 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
 
-const PinComponent = () => {
+import { AntDesign } from '@expo/vector-icons'
+
+interface Props {
+    id: string
+    image: string
+    title: string
+}
+
+const PinComponent = ({ image, title }: Props) => {
+    const [ratio, setRatio] = useState(3)
+    const onLike = () => {}
+
+    useEffect(() => {
+        if (image) {
+            Image.getSize(image, (width, height) => setRatio(width / height))
+        }
+    }, [image])
+
     return (
         <View style={styles.pin}>
-            <Image
-                source={{
-                    uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg',
-                }}
-                style={styles.image}
-            />
-            <Text style={styles.title}>notJust Dev Hoodie</Text>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={{
+                        uri: image,
+                    }}
+                    style={[styles.image, { aspectRatio: ratio }]}
+                />
+                <Pressable onClick={onLike} style={styles.icon}>
+                    <AntDesign name="hearto" size="large" color="black" />
+                </Pressable>
+            </View>
+            <Text numberOfLines={2} style={styles.title}>
+                {title}
+            </Text>
         </View>
     )
 }
@@ -20,15 +44,30 @@ export default PinComponent
 const styles = StyleSheet.create({
     pin: {
         width: '100%',
+        padding: 5,
+    },
+    imageContainer: {
+        position: 'relative',
+    },
+    icon: {
+        position: 'absolute',
+        bottom: 10,
+        right: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 35,
+        height: 35,
+        borderRadius: 17.5,
+        backgroundColor: '#e5e5e5',
     },
     image: {
         borderRadius: 25,
         width: '100%',
-        height: '100%',
     },
     title: {
-        fontSize: 20,
-        fontWeight: 'bold',
+        fontSize: 17,
+        fontWeight: '500',
         margin: 10,
     },
 })
