a275ff703987463883d77cd4d9dba62588c2b48f
natnael-OQP <makiya112545@gmail.com>
2022-05-15 21:11:32 +0300

    masonryList

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,30 @@
-import { StyleSheet } from 'react-native'
+import { FlatList, StyleSheet } from 'react-native'
 import PinComponent from '../components/PinComponent'
 
 import { Text, View } from '../components/Themed'
 import { RootTabScreenProps } from '../types'
+import Pins from '../assets/data/pins'
+import MasonryList from '@react-native-seoul/masonry-list'
+
+interface Props {
+    item: {
+        id: string
+        image: string
+        title: string
+    }
+}
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
     return (
         <View style={styles.container}>
-            <PinComponent />
+            <MasonryList
+                style={{ width: '100%' }}
+                data={Pins}
+                numColumns={2}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => <PinComponent {...item} />}
+                showsVerticalScrollIndicator={false}
+            />
         </View>
     )
 }
@@ -17,6 +34,7 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        padding: 10,
     },
     title: {
         fontSize: 20,
