d5ade7ab336a5d962fbaf119499039f5cae4af1f
natnael-OQP <makiya112545@gmail.com>
2022-05-17 23:12:08 +0300

    create createPin Screen

---
+++ screens/CreatePinScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CreatePinScreen = () => {
+    return (
+        <View>
+            <Text>CreatePinScreen</Text>
+        </View>
+    )
+}
+
+export default CreatePinScreen
+
+const styles = StyleSheet.create({})
