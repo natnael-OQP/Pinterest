7638268d1bcc20f2f5c1a98c2913d2a7cf3e76ad
natnael-OQP <makiya112545@gmail.com>
2022-05-17 18:45:20 +0300

    pinScreen created

--- screens/PinScreen.tsx
+++ screens/PinScreen.tsx
@@ -1,26 +1,82 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native'
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
+import { Ionicons } from '@expo/vector-icons'
 
-const PinScreen = () => {
+interface Props {
+    image: string
+    title: string
+}
+
+const PinScreen = ({ navigation, route: { params } }: any) => {
+    const [ratio, setRatio] = useState()
+    const { image, title } = params
+    const inset = useSafeAreaInsets()
+
+    useEffect(() => {
+        Image.getSize(image, (width: number, height: number) =>
+            setRatio(width / height)
+        )
+    }, [])
+
+    const goBack = () => {
+        navigation.goBack()
+    }
+
     return (
-        <View style={styles.container}>
-            <View style={styles.imageContainer}>
+        <SafeAreaView style={{ backgroundColor: '#000', height: '100%' }}>
+            <StatusBar style="light" />
+            <View style={styles.container}>
+                <Pressable
+                    onPress={goBack}
+                    style={[styles.icons, { top: inset.top + 20 }]}
+                >
+                    <Ionicons name="chevron-back" size={35} color="white" />
+                </Pressable>
                 <Image
-                    style={styles.image}
+                    style={[styles.image, { aspectRatio: ratio }]}
                     source={{
-                        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/3.jpeg',
+                        uri: image,
                     }}
                 />
+                <Text style={styles.title}> {title}</Text>
             </View>
-            <Text>PinScreen</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
 export default PinScreen
 
 const styles = StyleSheet.create({
-    container: {},
-    imageContainer: {},
-    image: {},
+    container: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 40,
+        borderTopRightRadius: 40,
+        overflow: 'hidden',
+    },
+    icons: {
+        position: 'absolute',
+        left: 10,
+        zIndex: 999,
+        width: 40,
+        height: 40,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 20,
+        backgroundColor: '#181818',
+    },
+    image: {
+        width: '100%',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        margin: 10,
+        textAlign: 'center',
+        height: '100%',
+    },
 })
