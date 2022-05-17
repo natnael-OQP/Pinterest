0d14467ed8b309c80832cc0926909ad7adbe30a5
natnael-OQP <makiya112545@gmail.com>
2022-05-17 18:43:32 +0300

    add pin type

--- types.tsx
+++ types.tsx
@@ -3,35 +3,35 @@
  * https://reactnavigation.org/docs/typescript/
  */
 
-import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs'
 import {
-  CompositeScreenProps,
-  NavigatorScreenParams,
-} from "@react-navigation/native";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
+    CompositeScreenProps,
+    NavigatorScreenParams,
+} from '@react-navigation/native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
 declare global {
-  namespace ReactNavigation {
-    interface RootParamList extends RootStackParamList {}
-  }
+    namespace ReactNavigation {
+        interface RootParamList extends RootStackParamList {}
+    }
 }
 
 export type RootStackParamList = {
-  Root: NavigatorScreenParams<RootTabParamList> | undefined;
-  Modal: undefined;
-  NotFound: undefined;
-};
+    Root: NavigatorScreenParams<RootTabParamList> | undefined
+    Pin: undefined
+    NotFound: undefined
+}
 
 export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
-  NativeStackScreenProps<RootStackParamList, Screen>;
+    NativeStackScreenProps<RootStackParamList, Screen>
 
 export type RootTabParamList = {
-  Home: undefined;
-  Profile: undefined;
-};
+    Home: undefined
+    Profile: undefined
+}
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> =
-  CompositeScreenProps<
-    BottomTabScreenProps<RootTabParamList, Screen>,
-    NativeStackScreenProps<RootStackParamList>
-  >;
+    CompositeScreenProps<
+        BottomTabScreenProps<RootTabParamList, Screen>,
+        NativeStackScreenProps<RootStackParamList>
+    >
