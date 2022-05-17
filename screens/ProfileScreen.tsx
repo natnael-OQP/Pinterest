fc995207f814a00984364f954dacc86e26c46f1b
natnael-OQP <makiya112545@gmail.com>
2022-05-17 22:59:33 +0300

    profile Screen created

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,12 +1,35 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Image, ScrollView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-import { Text, View } from '../components/Themed'
+import { Text } from '../components/Themed'
+import MasonryList from '@react-native-seoul/masonry-list'
+import PinComponent from '../components/PinComponent'
+import pins from '../assets/data/pins'
 
 export default function ProfileScreen() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>notJust Dev Hoodie</Text>
-        </View>
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <SafeAreaView style={styles.container}>
+                <Image
+                    style={styles.image}
+                    source={{
+                        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/7.jpeg',
+                    }}
+                />
+                <Text style={styles.title}>Natnael Abebe </Text>
+                <Text style={styles.subTitle}>
+                    213 Follower | 341 Followings{' '}
+                </Text>
+                <MasonryList
+                    style={{ width: '100%' }}
+                    data={pins}
+                    numColumns={2}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => <PinComponent {...item} />}
+                    showsVerticalScrollIndicator={false}
+                />
+            </SafeAreaView>
+        </ScrollView>
     )
 }
 
@@ -14,11 +37,21 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        // justifyContent: 'center',
         padding: 10,
+    },
+    image: {
+        width: 150,
+        height: 150,
+        borderRadius: 150 / 2,
     },
     title: {
         fontSize: 20,
+        fontWeight: 'bold',
+        margin: 20,
+    },
+    subTitle: {
+        color: '#181818',
         fontWeight: 'bold',
         margin: 10,
     },
